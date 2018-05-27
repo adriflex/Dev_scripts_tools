8161274932578cd8ba96818ca332570e43ab25b2
adriflex <rouquie.adrien@gmail.com>
2018-05-27 18:33:42 +0100

    include custom script

--- Photoshop/rectangle_output.jsx
+++ Photoshop/rectangle_output.jsx
@@ -1,58 +1,8 @@
 ﻿#target photoshop
 #include json2.js
+#include custom_script.js
+
 document = app.activeDocument;
-
-//repeat function
-if (!String.prototype.repeat) {
-  String.prototype.repeat = function (count) {
-    "use strict";
-    if (this == null)
-      throw new TypeError("ne peut convertir " + this + " en objet");
-    var str = "" + this;
-    count = +count;
-    if (count != count)
-      count = 0;
-    if (count < 0)
-      throw new RangeError("le nombre de répétitions doit être positif");
-    if (count == Infinity)
-      throw new RangeError("le nombre de répétitions doit être inférieur à l'infini");
-    count = Math.floor(count);
-    if (str.length == 0 || count == 0)
-      return "";
-    // En vérifiant que la longueur résultant est un entier sur 31-bit
-    // cela permet d'optimiser l'opération.
-    // La plupart des navigateurs (août 2014) ne peuvent gérer des
-    // chaînes de 1 << 28 caractères ou plus. Ainsi :
-    if (str.length * count >= 1 << 28)
-      throw new RangeError("le nombre de répétitions ne doit pas dépasser la taille de chaîne maximale");
-    var rpt = "";
-    for (var i = 0; i < count; i++) {
-      rpt += str;
-    }
-    return rpt;
-  }
-}
-
-
-//padstart function
-if (!String.prototype.padStart) {
-    String.prototype.padStart = function padStart(targetLength,padString) {
-        targetLength = targetLength>>0; //truncate if number or convert non-number to 0;
-        padString = String((typeof padString !== 'undefined' ? padString : ' '));
-        if (this.length > targetLength) {
-            return String(this);
-        }
-        else {
-            targetLength = targetLength-this.length;
-            if (targetLength > padString.length) {
-                padString += padString.repeat(targetLength/padString.length); //append to original to ensure we are longer than needed
-            }
-            return padString.slice(0,targetLength) + String(this);
-        }
-    };
-}
-
-
 
 //Changement unite
 app.preferences.rulerUnits = Units.PIXELS;
